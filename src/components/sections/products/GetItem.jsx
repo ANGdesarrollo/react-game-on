b63c56fc460d866013632a444be2ede910ea217b
Alexis Graff <alexisgraff123@gmail.com>
2022-09-23 03:56:06 -0300

    agregado de readme

--- src/components/sections/products/GetItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function GetItem(arrayRecieved) {
-    let is_ok = true;
-    return (
-        new Promise((res, rej) =>  {
-            is_ok ? res(arrayRecieved) : rej('error')
-        })
-    );
-};
